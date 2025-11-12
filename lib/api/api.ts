b99f781c09de29d8853fac28b1e863d1133c2baa
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2025-11-12 20:03:31 +0100

    new

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 
-const isLocal = process.env.NODE_ENV === "development";
+const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
-const baseURL = isLocal
-  ? process.env.NEXT_PUBLIC_API_URL + "/api" 
-  : process.env.NEXT_PUBLIC_API_URL;       
+
+// const isLocal = process.env.NODE_ENV === "development";
+
+// const baseURL = isLocal
+//   ? process.env.NEXT_PUBLIC_API_URL + "/api" 
+//   : process.env.NEXT_PUBLIC_API_URL;       
+
 
 
 export const nextServer = axios.create({
