9867006be24c56869b0e3bc9b728e6ee2dbfc6d0
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2025-11-12 20:06:22 +0100

    new

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+const isLocal = process.env.NODE_ENV === "development";
 
-
-// const isLocal = process.env.NODE_ENV === "development";
-
-// const baseURL = isLocal
-//   ? process.env.NEXT_PUBLIC_API_URL + "/api" 
-//   : process.env.NEXT_PUBLIC_API_URL;       
-
+const baseURL = isLocal
+  ? "http://localhost:3000/api"        // при разработке
+  : "https://notehub-api.goit.study";  // при продакшене (Vercel)
 
 
 export const nextServer = axios.create({
